8efeacc3068f72249698044acaf3a16f4ef6c358
Vikas Yadav <yadavikas13@gmail.com>
2023-02-09 12:23:10 +0530

    ss

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
-import { ethers, providers } from "ethers";
+import { ethers, Provider } from "ethers";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
